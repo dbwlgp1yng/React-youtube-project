c98112699204034482a3e8df634a90a20a0b4914
jihye yu <dbwlgp1yng@gmail.com>
2023-08-02 08:52:51 +0900

    feat: Header 검색 기능 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,35 @@
-import React from "react";
-import { BsSearch } from "react-icons/bs";
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { BsSearch, BsYoutube } from "react-icons/bs";
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 export default function Header() {
+  const { keyword } = useParams();
+  const [text, setText] = useState('');
   const navigate = useNavigate();
-  const handleClick = () => navigate('/');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/videos/${text}`);
+  };
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  useEffect(() => setText(keyword || ''), [keyword]); // keyword가 변경될때마다 검색어도 바뀌도록
 
   return (
-    <header className="fixed w-full flex items-center justify-between px-10 py-4">
-      <div onClick={handleClick} className="cursor-pointer">
-        <img
-          className="w-24"
-          src="https://logos-world.net/wp-content/uploads/2020/05/YouTube-Logo.jpg"
-          alt="youtube logo"
-        />
-      </div>
-      <form className="w-1/3 relative">
+    <header className="w-full flex p-4 border-b border-zinc-300 mb-4">
+      <Link to='/' className="flex items-center">
+        <BsYoutube className="text-4xl text-brand" />
+        <h1 className="font-bold text-3xl ml-2">Youtube</h1>
+      </Link>
+      <form className="w-full flex justify-center" onSubmit={handleSubmit}>
         <input 
-          className="w-full border-[1px] border-gray-500 rounded-3xl pl-4 pr-14 py-1 focus:outline-none" 
+          className="w-1/2 px-4 outline-none bg-black text-gray-50 rounded-3xl" 
           type="text" 
           placeholder="검색어를 입력하세요." 
+          value={text}
+          onChange={handleChange}
         />
-        <button className="absolute top-2 right-4">
+        <button className="px-4">
           <BsSearch />
         </button>
       </form>
