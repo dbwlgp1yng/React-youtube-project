35b6664c228f42b0db6cb10cace7f53dbd69a2b3
jihye yu <dbwlgp1yng@gmail.com>
2023-08-14 12:18:44 +0900

    refactor: VideoCard 리팩토링

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,32 +1,18 @@
 import React from "react";
-
+import { formatAgo } from "../util/date";
 
 export default function VideoCard({ video }) {
-  const title = video.snippet.title.length > 60 ? `${video.snippet.title.slice(0, 60)}...` : video.snippet.title;
-  const publishedDate = new Date(video.snippet.publishedAt);
-  const currentDate = new Date();
-  const timeDiffInMilliseconds = currentDate - publishedDate;
-  const hoursAgo = Math.floor(timeDiffInMilliseconds / (60 * 60 * 1000));
-  const daysAgo = Math.floor(timeDiffInMilliseconds / (24 * 60 * 60 * 1000));
-  const monthsAgo = Math.floor(timeDiffInMilliseconds / (30 * 24 * 60 * 60 * 1000));
-
-  let timeAgoText = '';
-  if (monthsAgo > 0) {
-    timeAgoText = `${monthsAgo} month ago`;
-  } else if (daysAgo > 0) {
-    timeAgoText = `${daysAgo} day ago`;
-  } else if (hoursAgo > 0) {
-    timeAgoText = `${hoursAgo} hour ago`;
-  } else {
-    timeAgoText = "right now";
-  }
+  const { title, thumbnails, publishedAt, channelTitle } = video.snippet;
 
   return (
-    <li className="w-1/5 p-2">
-      <img src={video.snippet.thumbnails.medium.url} alt="썸네일이미지" />
-      <h2 className="w-full font-bold">{title}</h2>
-      <p>{video.snippet.channelTitle}</p>
-      <p>{timeAgoText}</p>
+    <li>
+      <img className="w-full" src={thumbnails.medium.url} alt={title} /> 
+      <div>
+        <p className="font-semibold my-2 line-clamp-2">{title}</p>
+        <p className="text-sm opacity-80">{channelTitle}</p>
+        <p className="text-sm opacity-80">{formatAgo(publishedAt, 'ko')}</p>
+      </div>
     </li>
   );
 }
+  
